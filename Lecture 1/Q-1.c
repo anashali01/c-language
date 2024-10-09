227f9aa24f1f36b11ddf33e2cc82c3f32a966b28
anashali01 <saiyedanashali@gmail.com>
2024-10-09 13:39:35 +0530

    first commit

---
+++ Lecture 1/Q-1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(){
+    int num1=12,num2=6,result;
+    result=num1+num2;
+    printf("Addition of %d and %d is %d",num1,num2,result);
+    result=num1-num2;
+    printf("\nsubtraction of %d and %d is %d",num1,num2,result);
+    result=num1*num2;
+    printf("\nmultiplication of %d and %d is %d",num1,num2,result);
+    result=num1/num2;
+    printf("\ndivision of %d and %d is %d",num1,num2,result);
+    result=num1%num2;
+    printf("\nmodulus of %d and %d is %d",num1,num2,result);
+
+
+    result=num1+num2;
+    printf("\n%d + %d = %d",num1,num2,result);
+    result=num1-num2;
+    printf("\n%d - %d = %d",num1,num2,result);
+    result=num1*num2;
+    printf("\n%d * %d = %d",num1,num2,result);
+    result=num1+num2;
+    printf("\n%d / %d = %d",num1,num2,result);
+    result=num1+num2;
+    printf("\n%d % %d = %d",num1,num2,result);
+}
